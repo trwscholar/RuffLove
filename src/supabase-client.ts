7beeef506051cd334e344ad820df71bcae826b8b
JoshuaHosea <joshhosea15@gmail.com>
2025-09-23 23:15:37 +0800

    revert supabase

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// ✅ Your project details
+const supabaseUrl = "https://quodelyrvbgydzvnybmp.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1b2RlbHlydmJneWR6dm55Ym1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MDg0OTcsImV4cCI6MjA3NDE4NDQ5N30.2ycwhEWAShu7e3P9p7Jfg4Ne5zx0asLp9f8FrbFTNUE";
 
+// ✅ Create the client with your URL + anon key
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
